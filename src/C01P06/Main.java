d6e326e554b348b7c09cb77415a896bbc8e464b3
Dwight Francis <dfrancis0@gmail.com>
2014-09-09 14:20:50 -0400

    Add Gaussian addition method

--- src/C01P06/Main.java
+++ src/C01P06/Main.java
@@ -1,16 +1,18 @@
 package C01P06;
 
+// (Summation of a series) Write a program that displays the result of
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+
 public class Main {
 
     public static void main(String[] args) {
-	    int start = 1;
-        int cap = 9;
-        int result = 0;
+	    //int start = 1;
+        int end = 9;
 
-        for (int count = start; count <= cap; count++) {
-            result += count;
-        }
+        System.out.println(sumGauss(end));
+    }
 
-        System.out.println(result);
+    public static int sumGauss(int cap) {
+        return (cap * (cap + 1) / 2);
     }
 }
